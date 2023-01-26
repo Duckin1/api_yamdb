8f60c2cfbc894c405cf799c4b7e65e9c51e6e5a7
Duckin1 <almaz200@bk.ru>
2023-01-26 19:59:09 +0300

    add users/models

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,5 +1,66 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .enum import UserRoles
+
+
+
+LENGTH_TEXT = 35 # Надо перенести в settings
+
+class User(AbstractUser):
+    '''Класс пользователя.'''
+    username = models.CharField(
+        max_length=150,
+        verbose_name='Логин',
+        unique=True,
+        db_index=True,
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя пользователя',
+        blank=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        blank=True
+    )
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='email',
+        unique=True
+    )
+    bio = models.TextField(
+        verbose_name='Биография',
+        blank=True
+    )
+    role = models.CharField(
+        max_length=20,
+        verbose_name='Роль',
+        choices=UserRoles.choices(),
+        default=UserRoles.user.name
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.username[LENGTH_TEXT]
+
+    @property
+    def is_admin(self):
+        return self.role == UserRoles.admin.name
+
+    @property
+    def is_moderator(self):
+        return self.role == UserRoles.moderator.name
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.user.name
+
+
 
 
