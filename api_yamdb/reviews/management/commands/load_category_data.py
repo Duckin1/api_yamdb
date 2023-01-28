8481444307120bc49217dcdb0b4ca8a9110c7dc9
Владислав Дронов <vdronovvladislav@gmail.com>
2023-01-29 02:07:37 +0300

    Тут вроде все работает. На всякий случай пушу

---
+++ api_yamdb/reviews/management/commands/load_category_data.py
@@ -0,0 +1,15 @@
+from csv import DictReader
+from django.core.management import BaseCommand
+
+from reviews.models import Category
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open('static/data/category.csv', 'r') as csvfile:
+            reader = DictReader(csvfile)
+            print(reader)
+            for row in reader:
+                category = Category(id=row['id'], name=row['name'],
+                                    slug=row['slug'])
+                category.save()
